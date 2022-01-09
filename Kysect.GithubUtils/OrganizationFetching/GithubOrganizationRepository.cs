40860adbac2e92e23371c191ee2f99f634a94183
Fredi Kats <fredikats@outlook.com>
2022-01-09 15:18:02 +0300

    refactor: replace class GithubOrganizationRepository with record

--- Kysect.GithubUtils/OrganizationFetching/GithubOrganizationRepository.cs
+++ Kysect.GithubUtils/OrganizationFetching/GithubOrganizationRepository.cs
@@ -1,15 +1,3 @@
 ﻿namespace Kysect.GithubUtils.OrganizationFetching;
 
-public class GithubOrganizationRepository
-{
-    public string Path { get; set; }
-    public string OrganizationName { get; set; }
-    public string RepositoryName { get; set; }
-
-    public GithubOrganizationRepository(string path, string organizationName, string repositoryName)
-    {
-        Path = path;
-        OrganizationName = organizationName;
-        RepositoryName = repositoryName;
-    }
-}
+public record GithubOrganizationRepository(string Path, string OrganizationName, string RepositoryName);
