7c0854eb6559ac228fad296a1ba068a3a4a673b1
Fredi Kats <fredikats@outlook.com>
2022-02-12 11:01:07 +0300

    feat: add activity parser

---
+++ Kysect.GithubUtils/ActivityParser/ActivityProviders/GithubActivityProvider.cs
@@ -0,0 +1,73 @@
+﻿using System.Diagnostics;
+using System.Text.Json;
+
+namespace Kysect.GithubUtils
+{
+    public class GithubActivityProvider
+    {
+        private const string Url = "https://github-contributions.now.sh/api/v1/";
+
+        private readonly HttpClient _client;
+
+        public GithubActivityProvider()
+        {
+            _client = new HttpClient();
+        }
+
+        public async Task<ActivityInfo> GetActivityInfo(string username, DateTime? from = null, DateTime? to = null)
+        {
+            string response = await _client.GetStringAsync(Url + username);
+            var activityInfo = JsonSerializer.Deserialize<ActivityInfo>(response, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            return activityInfo.FilterValues(from, to);
+        }
+
+        public Dictionary<string, ActivityInfo> GetActivityInfo(IReadOnlyCollection<string> usernames, bool isParallel, DateTime? from = null, DateTime? to = null)
+        {
+            if (!isParallel)
+            {
+                return usernames
+                    .ToDictionary(username => username, username => GetActivityInfo(username, @from, to).Result);
+            }
+
+            return usernames
+                .AsParallel()
+                .ToDictionary(username => username, username => GetActivityInfo(username, @from, to).Result);
+        }
+
+        /// <summary>
+        /// Sometimes result is empty. We will try to resend request to ensure that we cannot fetch data.
+        /// </summary>
+        /// <param name="usernames"></param>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        /// <returns></returns>
+        public Dictionary<string, ActivityInfo> GetInfoWithRetry(IReadOnlyCollection<string> usernames, DateTime? from = null, DateTime? to = null)
+        {
+            int tryCount = 5;
+
+            Dictionary<string, ActivityInfo> result = new Dictionary<string, ActivityInfo>();
+
+            Debug.Print($"Start processing: {usernames.Count}");
+            for (int i = 0; i < tryCount && result.Count != usernames.Count; i++)
+            {
+                if (i != 0)
+                {
+                    Debug.Print($"Elements for processing left: {usernames.Count - result.Count}");
+                    Thread.Sleep(2000);
+                }
+
+                List<(string Username, ActivityInfo Result)> localResult = usernames
+                    .Where(u => !result.ContainsKey(u))
+                    .AsParallel()
+                    .Select(username => (username, GetActivityInfo(username, @from, to).Result))
+                    .Where(r => r.Result.Total > 0)
+                    .ToList();
+
+                localResult.ForEach(r => result[r.Username] = r.Result);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Kysect.GithubUtils/ActivityParser/ApiResponses/ActivityInfo.cs
@@ -0,0 +1,41 @@
+﻿namespace Kysect.GithubUtils
+{
+    //TODO: remove logic from DTO
+    public class ActivityInfo
+    {
+        //TODO: if we use this - we need to add filter for this value
+        //public YearActivityInfo[] Years { get; set; }
+        public ContributionsInfo[] Contributions { get; set; }
+
+        //TODO: optimization
+        public int Total => PerMonthActivity().Sum(a => a.Count);
+
+        public List<ContributionsInfo> PerMonthActivity()
+        {
+            //TODO: rework grouping
+            return Contributions
+                .GroupBy(c => c.Date.Month.ToString()+ "." + c.Date.Year.ToString())
+                .Select(c => new ContributionsInfo(c.Key, c.Sum(_ => _.Count)))
+                .ToList();
+        }
+
+        public ActivityInfo FilterValues(DateTime? from, DateTime? to)
+        {
+            IEnumerable<ContributionsInfo> result = Contributions;
+            if (from is not null)
+                result = result.Where(a => a.Date >= from);
+            if (to is not null)
+                result = result.Where(a => a.Date <= to);
+
+            return new ActivityInfo
+            {
+                Contributions = result.ToArray()
+            };
+        }
+
+        public int GetActivityForPeriod(DateTime from, DateTime to)
+        {
+            return FilterValues(@from, to).Total;
+        }
+    }
+}
