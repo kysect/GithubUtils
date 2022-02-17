4bb59e21ff06eff563b8a40ef0709bf4745efc09
Fredi Kats <fredikats@outlook.com>
2022-02-17 17:57:17 +0300

    feat: add fetch options support

---
+++ Kysect.GithubUtils/RepositoryFetching/RepositoryFetchOptions.cs
@@ -0,0 +1,19 @@
+﻿using LibGit2Sharp;
+
+namespace Kysect.GithubUtils;
+
+public class RepositoryFetchOptions
+{
+    public bool IgnoreMissedBranch { get; }
+    public CheckoutOptions CheckoutOptions { get; }
+
+    public RepositoryFetchOptions(bool ignoreMissedBranch, CheckoutOptions checkoutOptions)
+    {
+        IgnoreMissedBranch = ignoreMissedBranch;
+        CheckoutOptions = checkoutOptions;
+    }
+
+    public RepositoryFetchOptions() : this(false, new CheckoutOptions())
+    {
+    }
+}
