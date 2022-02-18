40b204c5c038afb9695d8d1ceccd3ad9277ff6ef
Fredi Kats <fredikats@outlook.com>
2022-02-19 00:30:27 +0300

    feat: add organization replication hub

---
+++ Kysect.GithubUtils/OrganizationReplicator/OrganizationReplicationHub.cs
@@ -0,0 +1,31 @@
+﻿namespace Kysect.GithubUtils.OrganizationReplicator;
+
+public class OrganizationReplicationHub
+{
+    private readonly string _rootDirectory;
+
+    public OrganizationReplicationHub(string rootDirectory)
+    {
+        ArgumentNullException.ThrowIfNull(rootDirectory);
+
+        _rootDirectory = rootDirectory;
+        Directory.CreateDirectory(rootDirectory);
+    }
+
+    public bool TryAddOrganization(string organization)
+    {
+        ArgumentNullException.ThrowIfNull(organization);
+
+        string organizationDirectoryPath = Path.Combine(_rootDirectory, organization);
+        if (Directory.Exists(organizationDirectoryPath))
+            return false;
+
+        Directory.CreateDirectory(organizationDirectoryPath);
+        return true;
+    }
+
+    public IReadOnlyCollection<string> GetOrganizationNames()
+    {
+        return Directory.EnumerateDirectories(_rootDirectory).ToList();
+    }
+}
