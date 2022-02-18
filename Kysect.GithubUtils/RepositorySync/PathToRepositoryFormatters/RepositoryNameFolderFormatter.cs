657968315c99e1ca9ea9c465630cc042c1de9633
Fredi Kats <fredikats@outlook.com>
2022-02-18 23:19:15 +0300

    refactor: rework folder structure

---
+++ Kysect.GithubUtils/RepositorySync/PathToRepositoryFormatters/RepositoryNameFolderFormatter.cs
@@ -0,0 +1,16 @@
+﻿namespace Kysect.GithubUtils;
+
+public class RepositoryNameFolderFormatter : IPathToRepositoryFormatter
+{
+    private readonly string _rootPath;
+
+    public RepositoryNameFolderFormatter(string rootPath)
+    {
+        _rootPath = rootPath;
+    }
+
+    public string FormatFolderPath(string username, string repository)
+    {
+        return Path.Combine(_rootPath, repository);
+    }
+}
