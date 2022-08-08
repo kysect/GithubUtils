ad05bef17ffe8669fea879d9770affd71267b937
Fredi Kats <fredikats@outlook.com>
2022-08-08 19:00:33 +0400

    feat: skip inviting for already invited or added users

---
+++ Kysect.GithubUtils/Inviting/InviteResult.cs
@@ -0,0 +1,9 @@
+﻿namespace Kysect.GithubUtils;
+
+public record InviteResult(
+    IReadOnlyCollection<string> Success,
+    IReadOnlyCollection<string> Failed,
+    IReadOnlyCollection<string> AlreadyAdded,
+    IReadOnlyCollection<string> AlreadyInvited,
+    IReadOnlyCollection<string> WithExpiredInvites,
+    Exception? Exception);
---
+++ Kysect.GithubUtils/Tools/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace Kysect.GithubUtils.Tools.Extensions;
+
+public static class EnumerableExtensions
+{
+    public static (IReadOnlyCollection<T> Mathced, IReadOnlyCollection<T> NotMatched) SplitBy<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+    {
+        List<T> first = new List<T>();
+        List<T> second = new List<T>();
+        foreach (T item in source)
+        {
+            if (predicate(item))
+            {
+                first.Add(item);
+            }
+            else
+            {
+                second.Add(item);
+            }
+        }
+        return (first, second);
+    }
+}
