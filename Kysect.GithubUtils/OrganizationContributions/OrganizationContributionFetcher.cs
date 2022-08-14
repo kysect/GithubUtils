52b5b2d3f140aefc2d7029dc89334ea0b15a4c2d
Fredi Kats <fredikats@outlook.com>
2022-08-14 22:49:01 +0400

    feat: implement organization contribution fetcher

---
+++ Kysect.GithubUtils/OrganizationContributions/OrganizationContributionFetcher.cs
@@ -0,0 +1,37 @@
+﻿using Octokit;
+using Serilog;
+
+namespace Kysect.GithubUtils.OrganizationContributions;
+
+public class OrganizationContributionFetcher
+{
+    private readonly GitHubClient _client;
+
+    public OrganizationContributionFetcher(GitHubClient client)
+    {
+        _client = client;
+    }
+
+    public async Task<List<OrganizationContributor>> FetchOrganizationStatistic(string organizationName)
+    {
+        Log.Information($"Start fetching contributors from {organizationName}");
+
+        IReadOnlyList<Repository> repositories = await _client.Repository.GetAllForOrg(organizationName);
+
+        List<RepositoryContributor> contributors = new List<RepositoryContributor>();
+        foreach (Repository repository in repositories)
+        {
+            Log.Debug($"Try fetch contributions from {repository.FullName}");
+            IReadOnlyList<RepositoryContributor> repositoryContributors = await _client.Repository.GetAllContributors(repository.Id);
+            contributors.AddRange(repositoryContributors);
+        }
+
+        return contributors
+            .GroupBy(c => c.Login)
+            .Select(g => new OrganizationContributor(g.Key, g.Sum(c => c.Contributions)))
+            .ToList();
+
+    }
+}
+
+public record OrganizationContributor(string Username, int Contributions);
