789c47bda1cbaa433e98519536a4b346d374516a
annchous <annchooousss@gmail.com>
2022-09-04 15:45:50 +0400

    fix: handle ForbiddenException

--- Kysect.GithubUtils/Inviting/InviteResult.cs
+++ Kysect.GithubUtils/Inviting/InviteResult.cs
@@ -1,8 +1,7 @@
 ﻿namespace Kysect.GithubUtils;
 
 public record InviteResult(
-    IReadOnlyCollection<string> Success,
-    IReadOnlyCollection<string> Failed,
+    IReadOnlyCollection<UserInviteResult> InviteResults,
     IReadOnlyCollection<string> AlreadyAdded,
     IReadOnlyCollection<string> AlreadyInvited,
     IReadOnlyCollection<string> WithExpiredInvites,
