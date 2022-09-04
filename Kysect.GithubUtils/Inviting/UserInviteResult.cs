322304cc25bb61f162a156d50a7efca25500c349
annchous <annchooousss@gmail.com>
2022-09-04 15:45:28 +0400

    feat: add UserInviteResult model

---
+++ Kysect.GithubUtils/Inviting/UserInviteResult.cs
@@ -0,0 +1,13 @@
+﻿namespace Kysect.GithubUtils;
+
+public record UserInviteResult(string Username, UserInviteResultType Result, string? Reason);
+
+public enum UserInviteResultType
+{
+    Success = 1,
+    Failed,
+    Skipped,
+    AlreadyAdded,
+    AlreadyInvited,
+    InvitationExpired,
+}
